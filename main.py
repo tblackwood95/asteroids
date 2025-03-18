6bb89b57cd55c6b815bfc423c020e2e08348fa49
tblackwood95 <tblackwood@live.com>
2025-03-18 17:20:37 -0230

    added left and right rotation to the player class

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
         
         
         screen.fill((0, 0, 0))   
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import *
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -19,3 +19,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
