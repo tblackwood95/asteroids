1435972f00dcb187cc23ccd995fc6e6c06ba93b9
tblackwood95 <tblackwood@live.com>
2025-03-18 16:53:28 -0230

    added code to make circular objects and the triangle shaped player

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.time.Clock
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
     
     while True:
         for event in pygame.event.get():
@@ -16,9 +20,11 @@ def main():
                 return
         
         
-        screen.fill((0, 0, 0))
-        
+        screen.fill((0, 0, 0))   
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
